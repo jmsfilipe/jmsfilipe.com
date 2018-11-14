c6c01a46cffc7857426fc30dc31bd42d3359aa44
jmsfilipe <jmsfilipe@gmail.com>
2018-11-14 18:46:58 +0000

    Update js indentation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,21 @@
 // typewritter animation
-$(function(){
-
-    $("#typed").typed({
-        strings: ["an AngularJS developer.", "a SASS lover.", "a web developer.", "Jorge."],
-        typeSpeed: 30,
-        showCursor: false,
-        callback: function(){
-            lift();
-        }
-    });
-    function lift(){
-        $(".head-text").addClass("lift-text");
-        $(".badge-container").addClass("lift-text");
+$(function() {
+  $("#typed").typed({
+    strings: [
+      "an AngularJS developer.",
+      "a SASS lover.",
+      "a web developer.",
+      "Jorge."
+    ],
+    typeSpeed: 30,
+    showCursor: false,
+    callback: function() {
+      lift();
     }
+  });
 
+  function lift() {
+    $(".head-text").addClass("lift-text");
+    $(".badge-container").addClass("lift-text");
+  }
 });

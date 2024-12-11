beb37d85e03f1e7426b980df4ba6efa93c6d0f97
jmsfilipe <jmsfilipe@gmail.com>
2024-12-11 11:25:39 +0000

    Add nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/content", "@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/content", "@nuxt/image"],
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+    },
+  },
 });
